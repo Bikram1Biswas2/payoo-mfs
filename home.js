580cf2e9553cc3a1fd3055bf23ddf247f328747f
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-19 00:09:53 +0530

    Add AddMoney implement successfully and make able to update the totalBalance and display on the scree

--- home.js
+++ home.js
@@ -1,4 +1,32 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
     console.log('Add Money Button clicked');
+
+    // get money to be added
+    const addedMoney = document.getElementById('input-amount-added').value
+    //console.log(addedMoney);
+
+    // get the pin
+    const getPin = document.getElementById('input-pin-Number').value
+    //console.log(getPin);
+
+    // 
+ 
+
+    if(getPin === '9876'){
+        const totalBalance = document.getElementById('available-balance').textContent
+        console.log(totalBalance);
+
+        const updatedBalance = Number(totalBalance) + Number(addedMoney);
+        console.log(updatedBalance);
+
+        // display total Balance
+        document.getElementById('available-balance').innerText = updatedBalance
+        
+            
+    } else{
+        alert('Wrong Pin');
+    }
+
+    
 })
